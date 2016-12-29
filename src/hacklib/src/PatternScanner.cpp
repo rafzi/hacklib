d868ed6f581600b1bc15e8a466a2b23698dc94e4
hairys <githairys@zcnoc.net>
2016-12-29 09:35:11 -0500

    const memoryMap

--- src/hacklib/src/PatternScanner.cpp
+++ src/hacklib/src/PatternScanner.cpp
@@ -320,7 +320,7 @@ const std::vector<hl::MemoryRegion>& hl::GetCodeRegions(const std::string& modul
         throw std::runtime_error("no such module");
     }
 
-    static auto memoryMap = hl::GetMemoryMap();
+    const static auto memoryMap = hl::GetMemoryMap();
     std::copy_if(memoryMap.begin(), memoryMap.end(), std::back_inserter(lut[moduleName]), [hModule](const hl::MemoryRegion& r){
         return r.hModule == hModule && r.protection == hl::PROTECTION_READ_EXECUTE;
     });
